fea228c3a677d023cea23dd20f76c2657bf65e95
Reiko <kurets11@gmail.com>
2023-06-01 22:05:34 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('POSTGRES_DATABASE'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
