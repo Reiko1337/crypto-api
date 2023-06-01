728dcc8b4217668ee4c43a1aa4b593bc01b1a332
Reiko <kurets11@gmail.com>
2023-06-01 22:43:19 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
 ALLOWED_HOSTS = ['.vercel.app']
 
